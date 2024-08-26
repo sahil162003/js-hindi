d01180cdcc142e7e8273eb59dccf4d01208c1fd2
sahil <158731860+sahil162003@users.noreply.github.com>
2024-08-26 15:17:29 +0000

    various variable

--- .devcontainer/o1_basics/01_variable.js
+++ .devcontainer/o1_basics/01_variable.js
@@ -1,4 +1,10 @@
 const accountid = 444568
 let accountEmail ="sahil@23445.com"
 var accountPassword = "1234"
+let accountState;
 accountCity ="jaipur"
+//accountid = 2//not allpwed
+accountCity ="mumbai"
+accountPassword ="12333"
+console.log(accountid);
+console.table([accountid,accountEmail,accountPassword,accountCity,accountState]);
