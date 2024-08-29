b2fc8c18dd7956c35747446d7ea6eaf93ac22642
sahil <158731860+sahil162003@users.noreply.github.com>
2024-08-29 00:43:20 +0000

    datatypes

---
+++ .devcontainer/o1_basics/02_Datatypes.js
@@ -0,0 +1,18 @@
+///alert(3+3)///it works in brower not in node js
+
+console.log(3+3);
+let age= 21
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null =>standalone value
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof "sahil");//string
+console
+
+console.log(typeof age);number
+console.log(typeof undefined);//undefined ,it is a datatype
+console.log(typeof null);//object
