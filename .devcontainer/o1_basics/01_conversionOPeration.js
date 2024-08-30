af13a1618bf46fe2cc1ae6a4fb95c3cfaf52c541
sahil <158731860+sahil162003@users.noreply.github.com>
2024-08-30 00:23:35 +0000

    negvalue

---
+++ .devcontainer/o1_basics/01_conversionOPeration.js
@@ -0,0 +1,10 @@
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+//**********************OPERATIONS */
+let value = 3
+let negvalue = -value
+console.log(2+2);
+console.log(typeof negvalue)
+console.log(negvalue)
